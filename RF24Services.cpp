e68560815bdc8776689cc92d75990d04f5c5faf6
Filip Richter <richter.filip@gmail.com>
2016-08-12 09:17:54 +0200

    Refactoring, changed API, Added connection handling that closes #1

--- RF24Services.cpp
+++ RF24Services.cpp
@@ -9,14 +9,12 @@
 RF24Services::RF24Services(RF24 &rf): _rf(rf)
 {
   _irq_pin = NO_IRQ;
-  default_service = 0;
 }
 
 RF24Services::RF24Services(RF24 &rf, int8_t irq_pin): _rf(rf)
 {
   _irq_pin = irq_pin;
-  pinMode(_irq_pin, INPUT);
-  default_service = 0;
+  pinMode(_irq_pin, INPUT_PULLUP);
 }
 
 void RF24Services::addService(void (*callback)(uint8_t *data, uint8_t len), uint8_t sid)
@@ -26,29 +24,20 @@ void RF24Services::addService(void (*callback)(uint8_t *data, uint8_t len), uint
   service_cnt++;
 }
 
-void RF24Services::setDefaultService(void (*callback)(uint8_t *data, uint8_t len))
-{
-  default_service = callback;
-}
-
-void RF24Services::setActCallbacks(void (*active)(), void (*inactive)()){
-  active_callback = active;
-  inactive_callback = inactive;
-}
-
-void RF24Services::setBaseAddress(uint64_t base){
-  base_adr = base;
-}
-
 void RF24Services::doWork()
 {
+  unsigned long t = millis();
+  if(((t - rxTime) > lostTime) && (connectionState = CONNECTED)){
+    connectionState = LOST;
+    if(lostCallback) lostCallback();
+  }
   if(!irqStatus()) return;
-  rx_time=millis();
   bool tx,fail,rx;
   uint8_t pipe;
   _rf.whatHappened(tx,fail,rx);
   if(rx){
-    if(active_callback) active_callback();
+    rxTime=t;
+    if(activeCallback) activeCallback();
     while ( _rf.available(&pipe) )
     {
       uint8_t len = _rf.getDynamicPayloadSize();
@@ -56,7 +45,11 @@ void RF24Services::doWork()
       _rf.read(buffer, len);
       handlePacket(buffer, len);
     }
-    if(inactive_callback) inactive_callback();
+    if(inactiveCallback) inactiveCallback();
+    if(connectionState==LOST){
+      connectionState = CONNECTED;
+      if(connectCallback) connectCallback();
+    }
   }
 }
 
@@ -67,11 +60,7 @@ void RF24Services::handlePacket(uint8_t buffer[], uint8_t len){
       return;
     }
   }
-  if(default_service!=0) default_service(buffer, len);
-}
-
-unsigned long RF24Services::rxTime(){
-  return rx_time;
+  if(defaultService!=0) defaultService(buffer, len);
 }
 
 inline bool RF24Services::irqStatus(){
@@ -80,12 +69,12 @@ inline bool RF24Services::irqStatus(){
 }
 
 bool RF24Services::send(uint8_t pipe, void *data, uint8_t len){
-  if(active_callback) active_callback();
+  if(activeCallback) activeCallback();
   boolean status = false;
   _rf.stopListening();
-  _rf.openWritingPipe(pipe+base_adr);
+  _rf.openWritingPipe(pipe+baseAddress);
   status = _rf.write(data, len);
   _rf.startListening();
-  if(inactive_callback) inactive_callback();
+  if(inactiveCallback) inactiveCallback();
   return status;
 }
--- RF24Services.h
+++ RF24Services.h
@@ -9,6 +9,8 @@
 #define SERVICES_COUNT  20
 #define BUFFER_LENGTH   33
 #define NO_IRQ          -1
+#define CONNECTED       true
+#define LOST            false
 
 #include "Arduino.h"
 #include "stdint.h"
@@ -20,26 +22,37 @@ class RF24Services
     RF24Services(RF24 &s);
     RF24Services(RF24 &s, int8_t irq_pin);
     void addService(void (*callback)(uint8_t *data, uint8_t len), uint8_t sid);
+    /*
     void setDefaultService(void (*callback)(uint8_t *data, uint8_t len));
     void setActCallbacks(void (*active)(), void (*inactive)());
+    void setConnectCallbacks(void (*connect)(), void (*lost)());
+    void setLostTime(unsigned long time);
+    */
     void doWork();
     bool send(uint8_t pipe, void *data, uint8_t len);
-    void setBaseAddress(uint64_t base);
+    //void setBaseAddress(uint64_t base);
     inline bool irqStatus();
-    unsigned long rxTime();
+    //unsigned long rxTime();
+
+    void (*defaultService)(uint8_t *data, uint8_t len)=0;
+    void (*activeCallback)()=0;
+    void (*inactiveCallback)()=0;
+    void (*connectCallback)()=0;
+    void (*lostCallback)()=0;
+
+    bool connectionState=LOST;
+    unsigned long lostTime=4000;
+
+    uint64_t baseAddress;
+    unsigned long rxTime;
+
   private:  
     void handlePacket(uint8_t buffer[], uint8_t len);
     RF24& _rf; 
     int8_t _irq_pin;
     void (*service_ptrs[SERVICES_COUNT])(uint8_t *data, uint8_t len);
-    void (*default_service)(uint8_t *data, uint8_t len);
-    void (*active_callback)();
-    void (*inactive_callback)();
     uint8_t service_ids[SERVICES_COUNT];
     uint8_t service_cnt=0;
     uint8_t buffer[BUFFER_LENGTH];
-    uint64_t base_adr;
-    unsigned long rx_time;
 };
-
 #endif
